df4a61f9511b136286543ddac9cd02c57b5469fb
ZeroFhx <erickflores170404@gmail.com>
2025-08-12 01:23:39 -0500

    Agregar funcionalidad para mostrar detalles del producto en un modal y permitir agregar productos al carrito

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ import products from "./data/products.js";
 
 const productsContainer = document.querySelector(".products");
 
+const productModal = document.getElementById("product-modal");
+const modalQuantityInput = document.getElementById("modal-quantity");
+const modalCloseButton = document.getElementById("modal-close-button");
+const productForm = document.getElementById("modal-form");
+
+const cart = [];
+
+let actualProduct = null;
+
 function renderProducts(products) {
   products.forEach((product) => {
     const productElement = document.createElement("article");
     productElement.classList.add("product");
+    productElement.setAttribute("data-id", product.id);
     productElement.innerHTML = `
       <img src="${product.image}" alt="Imagen de ${product.title}" />
       <div class="product-info">
@@ -18,7 +28,103 @@ function renderProducts(products) {
       </footer>
     `;
     productsContainer.appendChild(productElement);
+
+    productElement
+      .querySelector(".add-to-cart")
+      .addEventListener("click", (event) => {
+        event.stopPropagation();
+        addToCart(product.id);
+      });
+
+    productElement.addEventListener("click", () => {
+      showProductDetails(product.id);
+    });
   });
 }
 
 renderProducts(products);
+
+function showProductDetails(productId) {
+  const product = products.find((p) => p.id === productId);
+  if (product) {
+    actualProduct = productId;
+    modalQuantityInput.value = 1;
+    document.getElementById("modal-product-title").textContent = product.title;
+    document.getElementById("modal-product-description").textContent =
+      product.description;
+    document.getElementById("modal-product-image").src = product.image;
+    document.getElementById(
+      "modal-product-price"
+    ).textContent = `$${product.price.toFixed(2)}`;
+    productModal.showModal();
+  }
+}
+
+function addToCart(productId, quantity = 1) {
+  const product = products.find((p) => p.id === productId);
+
+  if (!product) return;
+
+  const cartItemExists = cart.find((item) => item.id === productId);
+
+  if (cartItemExists) {
+    cartItemExists.quantity += quantity;
+  } else {
+    cart.push({
+      id: product.id,
+      price: product.price,
+      quantity: 1,
+    });
+  }
+
+  updateCartCount();
+}
+
+function getTotalPrice(product) {
+  return +(product.price * product.quantity).toFixed(2);
+}
+
+function getTotalCartPrice() {
+  const total = cart.reduce((total, item) => total + getTotalPrice(item), 0);
+  return +total.toFixed(2);
+}
+
+function closeModalWithAnimation() {
+  productModal.classList.add("closing");
+  productModal.addEventListener(
+    "animationend",
+    (event) => {
+      if (event.animationName === "hide-modal") {
+        productModal.close();
+        productModal.classList.remove("closing");
+      }
+    },
+    { once: true }
+  );
+}
+
+modalCloseButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  closeModalWithAnimation();
+});
+
+productForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const quantity = parseInt(modalQuantityInput.value);
+  if (quantity > 0) {
+    addToCart(actualProduct, quantity);
+    closeModalWithAnimation();
+    console.log(cart);
+  }
+});
+
+productModal.addEventListener("click", (e) => {
+  if (e.target === productModal) {
+    closeModalWithAnimation();
+  }
+});
+
+function updateCartCount() {
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+  document.querySelector(".cart .count").textContent = cartCount;
+}
